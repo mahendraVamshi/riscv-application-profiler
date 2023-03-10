d487ea1da33f59807916017fb96e98a07edfdaca
Mahendra Vamshi A <mahendra298vamshi@gmail.com>
2023-03-10 18:38:25 +0530

    prints the not found matches

--- riscv_application_profiler/profiler.py
+++ riscv_application_profiler/profiler.py
@@ -8,6 +8,17 @@ def run(log, output):
 
         # Read the log file
         lines = logfile.readlines()
+        
+    for line in lines :
+        if re.match(pattern,line):
+            line_count=line_count+1
+            
+        else:
+            print(line,line_count)
+    
+            
+
+print("total number of lines = ", line_count)
 
     # Print the number of lines in the log file
     print("Number of lines in the log file: ", len(lines))
