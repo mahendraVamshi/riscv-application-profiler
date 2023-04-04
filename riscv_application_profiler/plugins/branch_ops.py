2ea24cbf8cb9824c6e74b38815faf88e8d30c1c3
Karthik B K <karthik.bk@incoresemi.com>
2023-04-04 18:07:16 +0530

    add plugins

---
+++ riscv_application_profiler/plugins/branch_ops.py
@@ -0,0 +1,62 @@
+# See LICENSE for licensing information.
+
+# this file is a plugin for riscv_application_profiler
+# this file classifies instructions into groups based on +ve/-ve branch offsets.
+# this file classifies instructions into 'long' and 'short' branches based on branch offsets.
+
+from riscv_isac.log import *
+
+def group_by_branch_offset(master_inst_list: list, branch_threshold: int = 0):
+    '''
+    Groups instructions based on the branch offset.
+
+    Args:
+        - master_inst_list: A list of InstructionEntry objects.
+        - branch_threshold: The threshold for a branch to be considered 'long'.
+
+    Returns:
+        - A tuple containing a dictionary with the operations as keys and a list of
+            InstructionEntry objects as values, and a dictionary with the operations as
+            keys and the number of instructions in each group as values.
+    '''
+    logger.info("Grouping instructions by branch offset.")
+    # Create a dictionary with the operations as keys
+    op_dict = {'long': [], 'short': []}
+    # for entry in master_inst_list:
+    #     if entry.instr is None:
+    #         continue
+    #     if entry.instr.operation == 'branches':
+    #         if entry.instr.offset > branch_threshold:
+    #             op_dict['long'].append(entry)
+    #         else:
+    #             op_dict['short'].append(entry)
+    counts = {op: len(op_dict[op]) for op in op_dict.keys()}
+    logger.info('Done.')
+    return (op_dict, counts)
+
+def group_by_branch_sign(master_inst_list: list):
+    '''
+    Groups instructions based on the sign bit of the branch offset.
+    
+    Args:
+        - master_inst_list: A list of InstructionEntry objects.
+    
+    Returns:
+        - A tuple containing a dictionary with the operations as keys and a list of
+            InstructionEntry objects as values, and a dictionary with the operations as
+            keys and the number of instructions in each group as values.
+    '''
+    logger.info("Grouping instructions by branch offset sign.")
+    # Create a dictionary with the operations as keys
+    op_dict = {'positive': [], 'negative': []}
+    # for entry in master_inst_list:
+    #     if entry.instr is None:
+    #         continue
+    #     if entry.instr.operation == 'branches':
+    #         if entry.instr.offset > 0:
+    #             op_dict['positive'].append(entry)
+    #         else:
+    #             op_dict['negative'].append(entry)
+    counts = {op: len(op_dict[op]) for op in op_dict.keys()}
+    logger.info('Done.')
+    return (op_dict, counts)
---
+++ riscv_application_profiler/plugins/instr_groups.py
@@ -0,0 +1,32 @@
+# See LICENSE for licensing information.
+
+# this file is a plugin for riscv_application_profiler
+# this file classifies instructions into groups based on the conditions defined by the user.
+
+from riscv_isac.log import *
+
+def group_by_operation(operations: list, master_inst_list: list):
+    '''
+    Groups instructions based on the operation.
+
+    Args:
+        - operations: A list of operations to group by.
+        - master_inst_list: A list of InstructionEntry objects.
+
+    Returns:
+        - A tuple containing a dictionary with the operations as keys and a list of
+            InstructionEntry objects as values, and a dictionary with the operations as
+            keys and the number of instructions in each group as values.
+    '''
+    logger.info("Grouping instructions by operation.")
+
+    # Create a dictionary with the operations as keys
+    op_dict = {op: [] for op in operations}
+    # for entry in master_inst_list:
+    #     if entry.instr is None:
+    #         continue
+    #     if entry.instr.operation in operations:
+    #         op_dict[entry.instr.operation].append(entry)
+    counts = {op: len(op_dict[op]) for op in operations}
+    logger.info('Done.')
+    return (op_dict, counts)
