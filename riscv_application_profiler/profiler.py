dc6df3d0aaec16718d5e50064c5272bf59415c2a
Karthik B K <karthik.bk@incoresemi.com>
2023-04-04 18:07:28 +0530

    add support for plugin based profiling

--- riscv_application_profiler/profiler.py
+++ riscv_application_profiler/profiler.py
@@ -5,6 +5,8 @@ import math
 from riscv_isac.data.rvopcodesdecoder import disassembler
 from riscv_isac.log import *
 from riscv_isac.plugins.spike import *
+from riscv_application_profiler.plugins import instr_groups
+from riscv_application_profiler.plugins import branch_ops
 
 def run(log, output, verbose):
     logger.debug("Entered run().")
@@ -27,71 +29,89 @@ def run(log, output, verbose):
         master_inst_list.append(temp_entry)
 
     logger.info("Done decoding instructions.")
+    logger.info("Starting to profile...")
 
-def old(log, disass, output):
-    '''
-    Parses the given log and generates meaningful information.
-    '''
+    # Grouping by operations
+    groups = [
+        'loads',
+        'stores',
+        'imm computes',
+        'imm shifts',
+        'reg computes',
+        'reg shifts',
+        'jumps',
+        'branches'
+    ]
+    op_dict1, counts1 = instr_groups.group_by_operation(groups, master_inst_list)
+
+    # Group by branch sizes
+    branch_threshold = 0
+    op_dict2, counts2 = branch_ops.group_by_branch_offset(master_inst_list, branch_threshold)
+
+    # Group by branch signs
+    op_dict3, counts3 = branch_ops.group_by_branch_sign(master_inst_list)
+
+# def old(log, disass, output):
+#     '''
+#     Parses the given log and generates meaningful information.
+#     '''
     
-    # Open the log file and read lines
-    with open(log, 'r') as logfile:
-        # Read the log file
-        lines = logfile.readlines()
+#     # Open the log file and read lines
+#     with open(log, 'r') as logfile:
+#         # Read the log file
+#         lines = logfile.readlines()
 
-    # Find all the matches
-    cl_matches_list=[]
-    for line in lines :
-        matches=re.findall(commitlog_regex,line)
-        cl_matches_list=cl_matches_list+matches
-    #cl_matches_list = [re.findall(commitlog_regex, line, re.M) for line in lines]
+#     # Find all the matches
+#     cl_matches_list=[]
+#     for line in lines :
+#         matches=re.findall(commitlog_regex,line)
+#         cl_matches_list=cl_matches_list+matches
+#     #cl_matches_list = [re.findall(commitlog_regex, line, re.M) for line in lines]
 
-    #for i in range(20):
-    #   print (cl_matches_list[2])
+#     #for i in range(20):
+#     #   print (cl_matches_list[2])
 
-    # Open the disassembly file and read lines
-    with open(disass, 'r') as disassfile:
-        # Read the disassembly file
-        lines = disassfile.readlines()
+#     # Open the disassembly file and read lines
+#     with open(disass, 'r') as disassfile:
+#         # Read the disassembly file
+#         lines = disassfile.readlines()
 
-    # Find all the matches
-    d_matches_list = [re.findall(disass_regex, line, re.M) for line in lines]
+#     # Find all the matches
+#     d_matches_list = [re.findall(disass_regex, line, re.M) for line in lines]
 
-    #print(cl_matches_list[10000][0][1][2:])
-    #new
+#     #print(cl_matches_list[10000][0][1][2:])
+#     #new
 
-    #print(len(cl_matches_list))
+#     #print(len(cl_matches_list))
 
-    load_count=0
-    reg_write_count=0
-    store_count=0
-    branch_count=0
-    jump_count=0
-    R_type_count=0
-    I_type_count=0
-    U_type_count=0
+#     load_count=0
+#     reg_write_count=0
+#     store_count=0
+#     branch_count=0
+#     jump_count=0
+#     R_type_count=0
+#     I_type_count=0
+#     U_type_count=0
 
-    for i in range (10):#len(cl_matches_list)):
+#     for i in range (10):#len(cl_matches_list)):
 
-        instruction= "{0:08b}".format(int(cl_matches_list[i][1][2:], 16))
+#         instruction= "{0:08b}".format(int(cl_matches_list[i][1][2:], 16))
         
-        if (len(instruction)!=32):
-            d=(32-len(instruction))
-            instruction=('0'*d)+instruction
-        print(instruction[25:32])
-        if (instruction[25:32]=='0000011'):
-            load_count+=1
-        elif(instruction[25:32]=='0100011'):
-            store_count+=1
-        elif(instruction[25:32]=='1100011'):
-            branch_count+=1
+#         if (len(instruction)!=32):
+#             d=(32-len(instruction))
+#             instruction=('0'*d)+instruction
+#         print(instruction[25:32])
+#         if (instruction[25:32]=='0000011'):
+#             load_count+=1
+#         elif(instruction[25:32]=='0100011'):
+#             store_count+=1
+#         elif(instruction[25:32]=='1100011'):
+#             branch_count+=1
 
-        elif(instruction[25:32]=='1101111'):
-            jump_count+=1
-        elif(instruction[24:31]=='0010011'):
-            I_type_count+=1
+#         elif(instruction[25:32]=='1101111'):
+#             jump_count+=1
+#         elif(instruction[24:31]=='0010011'):
+#             I_type_count+=1
             
 
-        #print(instsruction)
-
-
-    
+#         #print(instsruction)
