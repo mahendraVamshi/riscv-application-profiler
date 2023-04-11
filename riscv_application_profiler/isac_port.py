3649bf9d176fd0d2294f83d6736aae5b09a46dbf
Karthik B K <karthik.bk@incoresemi.com>
2023-04-11 17:05:38 +0530

    isac_port: disable any git repos in lib/

--- riscv_application_profiler/isac_port.py
+++ riscv_application_profiler/isac_port.py
@@ -29,6 +29,10 @@ def isac_setup_routine(lib_dir):
     shutil.copy(plugin_file,lib_dir)
     shutil.copy(constants_file,lib_dir)
 
-    isac_repo = repo_setup('https://github.com/riscv/riscv-opcodes.git', f'{lib_dir}/riscv_opcodes', 'master')
+    opcodes_repo = repo_setup('https://github.com/riscv/riscv-opcodes.git', f'{lib_dir}/riscv_opcodes', 'master')
 
+    # remove .git
+    shutil.rmtree(f'{lib_dir}/.git')
+
+    # add the library to the search path
     sys.path.append(lib_dir)
