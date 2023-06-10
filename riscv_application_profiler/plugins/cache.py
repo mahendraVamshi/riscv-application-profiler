ac00204c37319cd7210c4786ee2f0a57400714c1
vamshi <vamshi@LAPTOP-MTLHJ5FE>
2023-06-10 22:13:06 +0530

    minor optimization and cleaning done

--- riscv_application_profiler/plugins/cache.py
+++ riscv_application_profiler/plugins/cache.py
@@ -2,8 +2,6 @@ from cachesim import CacheSimulator, Cache, MainMemory
 import riscv_application_profiler.consts as consts
 
 def cache_simulator_example(master_inst_list, load_list, store_list):
-    print(len(load_list))
-    print(len(store_list))
 
     mem = MainMemory()
     l1 = Cache("L1", 64, 4, 64, "RR")  # 64 sets, 4-ways with cacheline size of 64 bytes
@@ -14,34 +12,25 @@ def cache_simulator_example(master_inst_list, load_list, store_list):
     for i in master_inst_list:
         if (i.reg_commit is not None):
             consts.reg_file[f'x{i.reg_commit[1]}'] = i.reg_commit[2]
+        if (i in load_list or i in store_list):
+            base = int(consts.reg_file[f'x{i.rs1[0]}'],16)
+            if ('d' in i.instr_name):
+                byte_length = 8
+            elif ('w' in i.instr_name):
+                byte_length = 4
+            elif ('h' in i.instr_name):
+                byte_length = 2
+            elif ('b' in i.instr_name):
+                byte_length = 1
+            if (i.imm is None):
+                address = base
+            else:
+                address = base+i.imm
         if (i in load_list):
-            base = int(consts.reg_file[f'x{i.rs1[0]}'],16)
-            if (i.imm is None):
-                address = base
-            else:
-                address = base+i.imm
-            if ('d' in i.instr_name):
-                cs.load(address, length=8)
-            if ('w' in i.instr_name):
-                cs.load(address, length=4)
-            if ('h' in i.instr_name):
-                cs.load(address, length=2)
-            if ('b' in i.instr_name):
-                cs.load(address, length=1)
-        if (i in store_list):
-            base = int(consts.reg_file[f'x{i.rs1[0]}'],16)
-            if (i.imm is None):
-                address = base
-            else:
-                address = base+i.imm
-            if ('d' in i.instr_name):
-                cs.store(address, length=8)
-            if ('w' in i.instr_name):
-                cs.store(address, length=4)
-            if ('h' in i.instr_name):
-                cs.store(address, length=2)
-            if ('b' in i.instr_name):
-                cs.store(address, length=1)
+            cs.load(address, byte_length)
+            
+        elif (i in store_list):
+            cs.store(address, byte_length)
 
     #cs.force_write_back()
     cs.print_stats()
