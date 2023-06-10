f6c4dda4a49823b5724c1be2f88eb4b793543932
vamshi <vamshi@LAPTOP-MTLHJ5FE>
2023-06-10 21:38:37 +0530

    added pycacchesim without errors

--- riscv_application_profiler/plugins/cache.py
+++ riscv_application_profiler/plugins/cache.py
@@ -2,29 +2,19 @@ from cachesim import CacheSimulator, Cache, MainMemory
 import riscv_application_profiler.consts as consts
 
 def cache_simulator_example(master_inst_list, load_list, store_list):
-    # #for i in range [0:10]:
-    #print(master_inst_list[0])
-    # print(load_list[1])
-    # print(load_list[2])
-    # print(load_list[3])
-    # print(load_list[4])
-    # print(store_list[0])
     print(len(load_list))
     print(len(store_list))
 
     mem = MainMemory()
     l1 = Cache("L1", 64, 4, 64, "RR")  # 64 sets, 4-ways with cacheline size of 64 bytes
+    mem.load_to(l1)
+    mem.store_from(l1)
     cs = CacheSimulator(l1, mem)
 
-    #matching_dicts = [d for d in my_list if specific_key in d]
-
     for i in master_inst_list:
-        if (i.reg_commit is None):
-            continue
-        else:
+        if (i.reg_commit is not None):
             consts.reg_file[f'x{i.reg_commit[1]}'] = i.reg_commit[2]
         if (i in load_list):
-            # consts.reg_file[f'x{i.reg_commit[1]}'] = i.reg_commit[2]
             base = int(consts.reg_file[f'x{i.rs1[0]}'],16)
             if (i.imm is None):
                 address = base
@@ -44,7 +34,6 @@ def cache_simulator_example(master_inst_list, load_list, store_list):
                 address = base
             else:
                 address = base+i.imm
-            
             if ('d' in i.instr_name):
                 cs.store(address, length=8)
             if ('w' in i.instr_name):
@@ -55,4 +44,6 @@ def cache_simulator_example(master_inst_list, load_list, store_list):
                 cs.store(address, length=1)
 
     #cs.force_write_back()
-    #cs.print_stats()
+    cs.print_stats()
+
+
