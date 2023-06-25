6affacd41cbd97e24edfe22a2b1026ae9909e7ba
vamshi <vamshi@LAPTOP-MTLHJ5FE>
2023-06-25 16:39:40 +0530

    fixed isac for compressed instr

--- riscv_application_profiler/plugins/cache.py
+++ riscv_application_profiler/plugins/cache.py
@@ -3,20 +3,21 @@ import riscv_application_profiler.consts as consts
 from riscv_isac.log import *
 
 def cache_simulator_example(master_inst_list, load_list, store_list):
-
+    # print(len(load_list))
+    # print(len(store_list))
     mem = MainMemory()
     l1 = Cache("L1", 64, 4, 64, "RR")  # 64 sets, 4-ways with cacheline size of 64 bytes
     mem.load_to(l1)
     mem.store_from(l1)
     cs = CacheSimulator(l1, mem)
-    for i in range(10):
-        logger.debug(print(load_list[i]))
+    # for i in range(10):
+    #     print(load_list[i])
     for i in master_inst_list:
         if (i.reg_commit is not None):
             consts.reg_file[f'x{i.reg_commit[1]}'] = i.reg_commit[2]
         if (i in load_list or i in store_list):
-            print(type(i.rs1))
-            print(i.__dict__)
+            # print(type(i.rs1))
+            # print(i.__dict__)
             base = int(consts.reg_file[f'x{i.rs1[0]}'],16)
             if ('d' in i.instr_name):
                 byte_length = 8
