ccd1db1f271635b67cea58b32a8fe466691781c6
Karthik B K <karthik.bk@incoresemi.com>
2023-07-25 23:21:01 +0530

    Update setup.py to 0.2.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = []
 
 setup(
     name='riscv-application-profiler',
-    version='0.1.0',
+    version='0.2.0',
     description="RISC-V Application Profiler",
     long_description=readme + '\n\n',
     classifiers=[
