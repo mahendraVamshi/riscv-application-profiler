225ecc79481ef76174db83b8ba0c0ff51c283e6e
vamshi <vamshi@LAPTOP-MTLHJ5FE>
2023-07-25 22:36:18 +0530

    updated isac_port back to initial

--- riscv_application_profiler/isac_port.py
+++ riscv_application_profiler/isac_port.py
@@ -20,21 +20,21 @@ def isac_setup_routine(lib_dir):
     Sets up the riscv-isac environment.
     '''
 
-    # os.makedirs(lib_dir, exist_ok=True)
+    os.makedirs(lib_dir, exist_ok=True)
 
-    # # Clone the riscv-isac repository
-    # isac_repo = repo_setup('https://github.com/riscv-software-src/riscv-isac.git', lib_dir, '0.17.0')
+    # Clone the riscv-isac repository
+    isac_repo = repo_setup('https://github.com/riscv-software-src/riscv-isac.git', lib_dir, '0.17.0')
 
-    # constants_file = os.path.join(os.getcwd(),  f'{lib_dir}/riscv_isac/data/constants.py')
-    # plugin_file = os.path.join(os.getcwd(),  f'{lib_dir}/riscv_isac/data/rvopcodesdecoder.py')
+    constants_file = os.path.join(os.getcwd(),  f'{lib_dir}/riscv_isac/data/constants.py')
+    plugin_file = os.path.join(os.getcwd(),  f'{lib_dir}/riscv_isac/data/rvopcodesdecoder.py')
 
-    # shutil.copy(plugin_file,lib_dir)
-    # shutil.copy(constants_file,lib_dir)
+    shutil.copy(plugin_file,lib_dir)
+    shutil.copy(constants_file,lib_dir)
 
-    # opcodes_repo = repo_setup('https://github.com/riscv/riscv-opcodes.git', f'{lib_dir}/riscv_opcodes', 'master')
+    opcodes_repo = repo_setup('https://github.com/riscv/riscv-opcodes.git', f'{lib_dir}/riscv_opcodes', 'master')
 
-    # # remove .git
-    # shutil.rmtree(f'{lib_dir}/.git')
+    # remove .git
+    shutil.rmtree(f'{lib_dir}/.git')
 
-    # # add the library to the search path
+    # add the library to the search path
     sys.path.append(lib_dir)
