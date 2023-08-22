debcf21107200d4dc984eaee32a786ffbb8e8930
vamshi <vamshi@LAPTOP-MTLHJ5FE>
2023-08-22 12:07:59 +0530

    added comments for readability in dependency plugin

--- riscv_application_profiler/plugins/dependency.py
+++ riscv_application_profiler/plugins/dependency.py
@@ -15,64 +15,94 @@ def raw_compute(master_inst_list: list):
             InstructionEntry objects as values, and a dictionary with the operations as
             keys and the number of instructions in each group as values.
     '''
+    # Initialize the process of computing register reads after writes.
     logger.info("Computing register reads after writes.")
-    reg_list=list(consts.reg_file.keys())
-    regs = {i : {'depth': 1} for i in reg_list}
-    ret_dict = {'Instructions': [], 'Depth': [], 'Count': []}
-    raw={}
-    instruction_list=[]
-    depth=[]
-    raw_list = []
-    prev_names = []  # Variable to store the previous entry.rd value
 
+    # Get a list of all registers in the register file.
+    reg_list = list(consts.reg_file.keys())
+
+    # Initialize a dictionary to hold register information, initially all with a depth of 1.
+    regs = {i: {'depth': 1} for i in reg_list}
+
+    # Initialize dictionaries to store results and raw data.
+    ret_dict = {'Instructions': [], 'Depth': [], 'Count': []}
+    raw = {}
+
+    # Initialize a list to store combined instructions.
+    instruction_list = []
+
+    # Initialize a list to store names of previously encountered registers.
+    prev_names = []
+
+    # Iterate through the list of instructions in master_inst_list.
     for entry in master_inst_list:
-        if (entry.rs1 is not None):
+        # Check if the instruction uses rs1 register.
+        if entry.rs1 is not None:
             name = str(entry.rs1[1]) + str(entry.rs1[0])
             instr = str(entry.instr_name)
+            
+            # Check if this register name was encountered before.
             if name in prev_names:
-                instruction=prev_instr+'  '+instr
+                instruction = prev_instr + '  ' + instr
+                
+                # Check if the combined instruction is in raw data.
                 if instruction in raw:
-                    if regs[name]['depth']==raw[instruction]['depth']:
+                    # Check if the register depth matches raw depth.
+                    if regs[name]['depth'] == raw[instruction]['depth']:
                         raw[instruction]['count'] += 1
                         prev_names.remove(name)
                         regs[name]['depth'] = 1
                 else:
-                    raw[instruction]= {'depth':regs[name]['depth'], 'count':1}
+                    raw[instruction] = {'depth': regs[name]['depth'], 'count': 1}
                     instruction_list.append(instruction)
                     prev_names.remove(name)
                     regs[name]['depth'] = 1
             else:
-                regs[name]['depth'] += 1   
-
-        if (entry.rs2 is not None):
+                regs[name]['depth'] += 1
+        
+        # Check if the instruction uses rs2 register.
+        if entry.rs2 is not None:
             name = str(entry.rs2[1]) + str(entry.rs2[0])
             instr = str(entry.instr_name)
+            
+            # Check if this register name was encountered before.
             if name in prev_names:
-                instruction=prev_instr+'  '+instr
+                instruction = prev_instr + '  ' + instr
+                
+                # Check if the combined instruction is in raw data.
                 if instruction in raw:
-                    if regs[name]['depth']==raw[instruction]['depth']:
+                    # Check if the register depth matches raw depth.
+                    if regs[name]['depth'] == raw[instruction]['depth']:
                         raw[instruction]['count'] += 1
                         prev_names.remove(name)
                         regs[name]['depth'] = 1
                 else:
-                    raw[instruction]= {'depth':regs[name]['depth'], 'count':1}
+                    raw[instruction] = {'depth': regs[name]['depth'], 'count': 1}
                     instruction_list.append(instruction)
                     prev_names.remove(name)
                     regs[name]['depth'] = 1
             else:
-                regs[name]['depth'] += 1 
-            
-        if (entry.rd is not None):
+                regs[name]['depth'] += 1
+        
+        # Check if the instruction defines a destination register (rd).
+        if entry.rd is not None:
             name = str(entry.rd[1]) + str(entry.rd[0])
             prev_instr = str(entry.instr_name)
+            
+            # Check if this register name was encountered before.
             if name not in prev_names:
                 prev_names.append(name)
             else:
                 regs[name]['depth'] = 1
-    
-    
+
+    # Populate the result dictionary from raw data.
     for entry in raw:
         ret_dict['Instructions'].append(entry)
         ret_dict['Count'].append(raw[entry]['count'])
         ret_dict['Depth'].append(raw[entry]['depth'])
-    return (ret_dict)
+
+    # Log the completion of the computation.
+    logger.info("Done")
+
+    # Return the result dictionary.
+    return ret_dict
